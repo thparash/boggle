71bbbb365e6c91a7c16a9a19dbf9d89dfd0411cd
Parash Thapa <parasht@gmail.com>
2019-12-25 13:24:17 +0545

    Remove pid directory from .dockerignore

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-# rm -f /app/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 
 RAILS_ENV=production rails db:migrate
 bundle exec puma -C config/puma.rb
